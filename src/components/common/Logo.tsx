93a6fb641f5f1eafff6ecb82d0f6ff5c39da83c6
AugustineBlewu1 <blewudziaugustine@gmail.com>
2025-11-29 11:43:14 +0000

    feat: maintenence mode

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -12,11 +12,11 @@ const Logo = () => {
       onClick={handleClick}
     >
       <img src={"/images/nic-logo.png"} alt="NIC Logo" />
-      <div className="">
+      {/* <div className="">
         <h1 className=" text-sm font-semibold leading-tight">NATIONAL</h1>
         <h1 className=" text-sm font-semibold leading-tight">INSURANCE</h1>
         <h1 className=" text-sm font-semibold leading-tight">COMMISSION</h1>
-      </div>
+      </div> */}
     </div>
   );
 };
